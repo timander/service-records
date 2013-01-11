f4dd1e8fac3024122215e5e62b1cdc6d041a863e
Tim Andersen <timander@gmail.com>
2013-01-11 09:04:13 -0600

    added fitnesse + fitlibrary

---
+++ src/test/java/net/timandersen/fixtures/ServiceRecordsDoFixture.java
@@ -0,0 +1,27 @@
+package net.timandersen.fixtures;
+
+import fitlibrary.DoFixture;
+
+public class ServiceRecordsDoFixture extends DoFixture {
+
+    public boolean getNetDotTimandersenDotFixturesDotServiceRecordsDoFixture(){
+        return true;
+    }
+
+    public boolean given(String what) {
+
+        return true;
+    }
+
+
+    public boolean when(String what) {
+
+        return true;
+    }
+
+    public boolean then(String what) {
+
+        return true;
+    }
+
+}
